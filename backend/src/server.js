4103a6ac82fee0dff3ca361d516890a713fc97fe
Divya <divyajyothi139@gmail.com>
2026-01-23 03:10:15 +0800

    make code ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,22 @@ import connectDB from './config/db.js';
 import notesRouter from './routes/notesRouter.js';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
+//import { fileURLToPath } from 'url';
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
+
+if (process.env.NODE_ENV !== "production") {
 app.use(cors({
   origin: 'http://localhost:5173', // Adjust according to your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }))
+}
 
 app.use(express.json());
 
@@ -21,6 +27,16 @@ app.use(rateLimiter);
 
 // Mount notes router
 app.use('/api/notes', notesRouter);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 
 // Start server after DB connection
 const startServer = async () => {
